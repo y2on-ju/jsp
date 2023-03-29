9081bd0908a0c088e310a188c96de7cf5ef7ac8b
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-03-29 15:38:49 +0900

    Create AppListener.java

---
+++ src/main/java/com/study/listener/AppListener.java
@@ -0,0 +1,37 @@
+package com.study.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class AppListener
+ *
+ */
+@WebListener
+public class AppListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public AppListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+       ServletContext application = sce.getServletContext();
+       application.setRequestCharacterEncoding("utf-8");
+    }
+	
+}
