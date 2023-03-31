7659c0898c25c633d44b4f1cc8b6b0ffb99fd546
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-03-31 17:48:44 +0900

    Create Bean01.java

---
+++ src/main/java/com/study/ch05/bean/Bean01.java
@@ -0,0 +1,28 @@
+package com.study.ch05.bean;
+
+public class Bean01 {
+	private String name;
+	private int age;
+	private boolean married;
+	
+	//기본생성자 있음
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isMarried() {
+		return married;
+	}
+	public void setMarried(boolean married) {
+		this.married = married;
+	}
+}
