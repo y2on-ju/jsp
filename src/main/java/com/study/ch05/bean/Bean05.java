4c4d7d00ad1338b4548765dcd36dd9613f8fcf8d
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-04 17:45:13 +0900

    Create Bean05.java

---
+++ src/main/java/com/study/ch05/bean/Bean05.java
@@ -0,0 +1,25 @@
+package com.study.ch05.bean;
+
+import java.util.*;
+
+public class Bean05 {
+	private String name;
+	private List<String> items;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getItems() {
+		return items;
+	}
+
+	public void setItems(List<String> items) {
+		this.items = items;
+	}
+
+}
