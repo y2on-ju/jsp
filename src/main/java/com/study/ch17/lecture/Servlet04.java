f0143da0d993597fdac384b5997383ff16bfb50c
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-07 22:05:19 +0900

    Create Servlet04.java

---
+++ src/main/java/com/study/ch17/lecture/Servlet04.java
@@ -0,0 +1,45 @@
+package com.study.ch17.lecture;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet04
+ */
+@WebServlet("/lec/sample04")
+public class Servlet04 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet04() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		// view로 포워드
+		String view = "/ch17/lecture/view02.jsp";
+		request.getRequestDispatcher(view)
+			.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
