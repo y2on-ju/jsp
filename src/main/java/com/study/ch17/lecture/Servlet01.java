21d39d01c472fd42d233c92da80553edbe22cefd
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-07 22:05:32 +0900

    Create Servlet01.java

---
+++ src/main/java/com/study/ch17/lecture/Servlet01.java
@@ -0,0 +1,39 @@
+package com.study.ch17.lecture;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class Servlet01
+ */
+@WebServlet("/lec/sample01")
+public class Servlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet01() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("get 요청 받음");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
