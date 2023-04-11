b4b90be8fabede571961a9faef1ac5451d838f8d
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-11 17:32:32 +0900

    Create Servlet17.java

---
+++ src/main/java/com/study/ch19/lecture/Servlet17.java
@@ -0,0 +1,42 @@
+package com.study.ch19.lecture;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet17
+ */
+@WebServlet("/lec/sample17")
+public class Servlet17 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet17() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("17번 서블릿 일함 *****");
+		
+		response.getWriter().println("17th servlet(*(&(*&(*&");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
