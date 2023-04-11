53aa3f9e0d3fe701ce9e64846d3eae5bd7d87de0
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-11 17:32:37 +0900

    Create Servlet15.java

---
+++ src/main/java/com/study/ch19/lecture/Servlet15.java
@@ -0,0 +1,41 @@
+package com.study.ch19.lecture;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet15
+ */
+@WebServlet("/lec/sample15")
+public class Servlet15 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet15() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("15번 서블릿일함~");
+		response.getWriter().println("15th servlet is working~~");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
