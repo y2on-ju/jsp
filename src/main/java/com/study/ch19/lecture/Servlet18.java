acb68b1716ff47905a2d337d7ee9707c38defa20
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-11 17:32:30 +0900

    Create Servlet18.java

---
+++ src/main/java/com/study/ch19/lecture/Servlet18.java
@@ -0,0 +1,41 @@
+package com.study.ch19.lecture;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet18
+ */
+@WebServlet("/lec/sample18")
+public class Servlet18 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet18() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("18번 서블릿 일함@#$@#$");
+		response.getWriter().println("18th servlet!!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
