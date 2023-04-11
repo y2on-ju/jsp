57c55d9d228b9edbae46f194e06d7c1328b11bb6
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-11 17:33:30 +0900

    Create Filter02.java

---
+++ src/main/java/com/study/ch19/lecture/Filter02.java
@@ -0,0 +1,55 @@
+package com.study.ch19.lecture;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class Filter02
+ */
+@WebFilter("/lec/sample14")
+public class Filter02 extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public Filter02() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		System.out.println("필터2 일함!!");
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+		
+		System.out.println("응답하는중");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
